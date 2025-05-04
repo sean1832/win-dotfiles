2c8bd72e162538933203b06a1c714c1071944f33
sean1832 <sean1832725142@gmail.com>
2025-05-05 00:59:04 +1000

    Remove restore-msstore.ps1 script as it is no longer needed

--- src/restore-msstore.ps1
+++
@@ -1,8 +0,0 @@
-# Self-elevate if not running as administrator
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "Not running as Administrator. Relaunching with elevation..." -ForegroundColor Yellow
-    Start-Process powershell "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-    exit
-}
-
-wsreset -i
